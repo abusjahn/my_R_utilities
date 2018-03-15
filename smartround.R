880efbd721a153c3cd20eb84d4e228d891ee2301
abusjahn <abusjahn@healthtwist.de>
2018-03-15 14:54:59 +0100

    Update smartround.R

--- smartround.R
+++ smartround.R
@@ -1,32 +1,38 @@
-smartround <- function(roundin,smooth=F,level=2, textout=T)
+roundR <- function(roundin,smooth=F,level=2, textout=T,drop0=F)
 {
   if (!is.matrix(roundin))
   {
     roundin<-matrix(roundin)
   }
-  smartround<-roundin
+  roundout<-roundin
   roundlevel<-0
   if(min(roundin,na.rm=T)!=0 | max(roundin,na.rm=T)!=0)
   {
     roundlevel<-round(max(0,level-log10(max(abs(roundin),
                                           na.rm=T))))
   }
-  smartround[which(!is.na(smartround))]<-
+  roundout[which(!is.na(roundout))]<-
     round(roundin[which(!is.na(roundin))],roundlevel)
-  if (smooth&max(abs(smartround),na.rm=T)!=0)
+  # if(all((roundin[which(!is.na(roundin))]-roundout[which(!is.na(roundout))])==0))
+  # {
+  #   roundlevel<-0
+  #   roundout[which(!is.na(roundout))]<-
+  #     round(roundin[which(!is.na(roundin))],roundlevel)
+  # }
+  if (smooth&max(abs(roundout),na.rm=T)!=0)
   {
-    smartround[which(!is.na(smartround))]<-
+      roundout[which(!is.na(roundout))]<-
       round(
-        smartround[which(!is.na(smartround))]/
-           10^round(log10(max(abs(smartround),na.rm=T))-level))*
-       10^round(log10(max(abs(smartround),na.rm=T))-level)
+        roundout[which(!is.na(roundout))]/
+           10^round(log10(max(abs(roundout),na.rm=T))-level))*
+       10^round(log10(max(abs(roundout),na.rm=T))-level)
   }
   if(textout==T)
   {
-    smartround[which(!is.na(smartround))]<-
-      formatC(smartround[which(!is.na(smartround))],format='f',
-              digits=roundlevel,drop0trailing=F)
+    roundout[which(!is.na(roundout))]<-
+      formatC(roundout[which(!is.na(roundout))],format='f',
+              digits=roundlevel,drop0trailing=drop0)
   }
-  return(smartround)
+  return(roundout)
 
 }
